3f45999454d589a9d06c8c3f00f0395a1d5844b0
ZeroSystems <sales@erpzero.com>
2019-10-09 21:57:02 +0200

    Add files via upload

---
+++ zero_attendance_base/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Zero Contract Workhours',
+    'version': '12.0.1',
+    'author': 'Zero Systems',
+    'company': 'Zero for Information Systems',
+    'website': "https://www.erpzero.com",
+    'email': "sales@erpzero.com",
+    'category': "Human Resources",
+    "description": """""",
+    "depends": [
+        'hr_contract'
+    ],
+    "demo": [
+    ],
+    "data": [
+        'views/hr_contract.xml',
+    ],
+    'images': ['static/description/logo.PNG'],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
