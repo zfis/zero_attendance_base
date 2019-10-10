315513839a3892405ff7f611320137d356580709
ZeroSystems <sales@erpzero.com>
2019-10-10 04:01:03 +0200

    Update __manifest__.py

--- zero_attendance_base/__manifest__.py
+++ zero_attendance_base/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '12.1.0',
     "summary": "base hr addon for Zero Systems HR",
     'description': """this addon is the base addon for Zero Systems Hr addons.test on odoo community and enterprise-Support English and Arabic interface""",
-    'depends': ['hr_holidays_public', 'zero_contract_work_hours'],
+    'depends': ['zero_contract_work_hours'],
     'category': 'Human Resources',
     'author': 'Zero Systems',
     'company': 'Zero for Information Systems',
