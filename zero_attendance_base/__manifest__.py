c21adfe0f3b8ade86e552ce14a4f4a0a7e3bc6cd
ZeroSystems <sales@erpzero.com>
2019-10-10 19:47:48 +0200

    Update __manifest__.py

--- zero_attendance_base/__manifest__.py
+++ zero_attendance_base/__manifest__.py
@@ -1,23 +1,24 @@
+# -*- coding: utf-8 -*-
 {
-    'name':'Zero Attendance Base',
-    'version':'12.0.1',
-    'author':'Zero Systems',
-    'company':'Zero for Information Systems',
-    'website':'https://www.erpzero.com',
-    'email': 'sales@erpzero.com',
-    'category':'Human Resources',
-    "description": """""",
-    'depends': [
-        'hr_holidays_public',
-        'hr_attendance',
-        'zero_contract_work_hours',
-    ],
-    "demo": [
-    ],
-    "data": [
+    'name': 'Zero Attendance Base',
+    'version': '1.0',
+    "summary": "base hr addon for Zero Systems HR",
+    'description': """--((Must Install hr_holidays_public  from https://apps.odoo.com/apps/modules/12.0/hr_holidays_public/))
+    before installing this addon---
+    
+    this addon is the base addon for Zero Systems Hr addons.
+    
+    tested on odoo community and enterprise-Support English and Arabic interface""",
+    'depends': ['hr_attendance','zero_contract_work_hours'],
+    'category': 'Human Resources',
+    'author': 'Zero Systems',
+    'company': 'Zero for Information Systems',
+    'website': "https://www.erpzero.com",
+    'email': "sales@erpzero.com",
+    'data': [
     ],
     'images': ['static/description/logo.PNG'],
-    "installable": True,
-    "auto_install": False,
-    "application": True,
+    'installable': True,
+    'auto_install': False,
+    'application': True,
 }
