81cd12f0df169383772123fa13caba6e7379e179
ZeroSystems <sales@erpzero.com>
2019-10-11 03:10:41 +0200

    Update __manifest__.py

--- zero_attendance_base/__manifest__.py
+++ zero_attendance_base/__manifest__.py
@@ -6,7 +6,8 @@
     'description': """--((Must Install hr_holidays_public  from https://apps.odoo.com/apps/modules/12.0/hr_holidays_public/))
     before installing this addon---
     
-    this addon is the base addon for Zero Systems Hr addons.
+    this addon is the base addon for Zero Systems Hr addons. aim to Counts the missing/un-attended workday hours that the employee took in a workday he/she actually was present on,
+        but he/she took no permission/leave for those hours
     
     tested on odoo community and enterprise-Support English and Arabic interface""",
     'depends': ['hr_attendance','zero_contract_work_hours'],
